4bbd6b4751e72c17df93f9def445e50a8fe377c3
Sarah Deakin <sarahjarquedeakin@gmail.com>
2022-03-08 20:58:48 -0500

    lets goooooo

--- index.js
+++ index.js
@@ -10,17 +10,26 @@ app.listen(process.env.PORT, () => console.log("Server Start at 5000 Port"));
 
 app.use(express.static("public"));
 app.use(cors());
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin"); // update to match the domain you will make the request from
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
 
 app.get("/songs", alldata);
 function alldata(request, response) {
   // Returns all information about the elements
   response.send(songs);
+}
+
+app.get("/songs/:song/", searchSongs);
+function searchSongs(request, response) {
+  var word = request.params.song;
+  word = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  console.log(word);
+  //console.log(elements[word]);
+  if (songs[word]) {
+    var reply = songs[word];
+  } else {
+    var reply = {
+      status: "Not Found",
+    };
+  }
+  console.log(reply.boil);
+  response.send(reply);
 }
