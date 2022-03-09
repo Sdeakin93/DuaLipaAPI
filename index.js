6467014332d8279d44b5ac506b812163f9edb3ac
Sarah Deakin <sarahjarquedeakin@gmail.com>
2022-03-08 20:35:32 -0500

    YOU GOT ME IN CORS AGAIN

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 var fs = require("fs");
 var data = fs.readFileSync("data.json");
 var songs = JSON.parse(data);
+
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.listen(process.env.PORT, () => console.log("Server Start at 5000 Port"));
 
 app.use(express.static("public"));
+app.use(cors());
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin"); // update to match the domain you will make the request from
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
 app.get("/songs", alldata);
 function alldata(request, response) {
   // Returns all information about the elements
