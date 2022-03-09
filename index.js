48453ed9c8212719b1cd657d7ba8c671770299c2
Sarah Deakin <sarahjarquedeakin@gmail.com>
2022-03-08 20:11:13 -0500

    port add

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.warn(`App listening on http://localhost:${PORT}/songs`);
-});
+process.env.PORT || port;
 
 app.use(express.static("public"));
 app.use(cors());
